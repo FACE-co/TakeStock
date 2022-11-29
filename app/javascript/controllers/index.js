182211da86871326c1ac9ca06d7583bf61e7bda0
Ambery Harris <amberyharris.ah@gmail.com>
2022-11-29 18:00:47 +1100

    added stimulus dropdown, prep for turbo rendering portfolios in root page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import DropdownController from "./dropdown_controller"
+application.register("dropdown", DropdownController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
