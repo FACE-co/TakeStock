461d43e4e7eca03baddab4878b4130ffb8a12e1b
Ambery Harris <amberyharris.ah@gmail.com>
2022-12-05 17:47:21 +1100

    twitter render restructured to turbo div

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,13 @@
 class TweetsController < ApplicationController
   def index
     @stock = Stock.friendly.find(params[:stock_id])
-    @tweets = get_tweet_ids(@stock, 10)
+    @tweets = get_tweet_ids(@stock, 100)
   end
 
   private
 
   def get_tweet_ids(stock, max_results)
-    url = URI("https://api.twitter.com/2/tweets/search/recent?query=#{stock.ticker}&max_results=#{max_results}&tweet.fields=public_metrics,lang,entities&expansions=author_id&user.fields=public_metrics&sort_order=relevancy")
+    url = URI("https://api.twitter.com/2/tweets/search/recent?query=#{stock.ticker}&max_results=#{max_results}&tweet.fields=public_metrics,lang,entities,referenced_tweets&expansions=author_id&user.fields=public_metrics&sort_order=relevancy")
 
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
@@ -19,9 +19,9 @@ class TweetsController < ApplicationController
     response = https.request(request).read_body.force_encoding("UTF-8")
     response_json = JSON.parse(response)
     response_array = response_json["data"] # this returns an array of hashes
-    filtered_response_array = response_array.select { |e| (e["entities"]["cashtags"].nil? || e["entities"]["cashtags"].count <= 3) && e["lang"] == "en" }
+    filtered_response_array = response_array.select { |e| (e["entities"].nil?) || (e["entities"]["cashtags"].nil? || e["entities"]["cashtags"].count <= 3) && (e["lang"] == "en") && (e["entities"]["urls"].nil?) }
     id_array = filtered_response_array.map { |hash| hash["id"] }
 
-    return id_array
+    return [id_array.first(10), filtered_response_array]
   end
 end
