33a98290c14298e2e76e0e854eb4805a5072c0fe
Elliott Kan <elliottkan7@gmail.com>
2022-12-05 14:49:40 +1100

    Logic Complete for Algolia Test

--- hello_algolia_TESTFILE.rb
+++ hello_algolia_TESTFILE.rb
@@ -3,6 +3,49 @@ require 'pry-byebug'
 require 'json'
 
 
+# TEST RECORDS
+records = [
+  {
+    name: "APPLE INC",
+    ticker: "AAPL",
+    cik: "320193",
+    cusip: "037833100",
+    exchange: "NASDAQ",
+    isDelisted: false,
+    category: "Domestic Common Stock",
+    sector: "Technology",
+    industry: "Consumer Electronics",
+    sic: "3571",
+    sicSector: "Manufacturing",
+    sicIndustry: "Electronic Computers",
+    famaSector: "",
+    famaIndustry: "Computers",
+    currency: "USD",
+    location: "California; U.S.A",
+    api_id: "a43c3ffca9b4a0be9cee4fa1120832a2"
+  },
+  {
+    name: "TESLA INC",
+    ticker: "TSLA",
+    cik: "1318605",
+    cusip: "88160R101",
+    exchange: "NASDAQ",
+    isDelisted: false,
+    category: "Domestic Common Stock",
+    sector: "Consumer Cyclical",
+    industry: "Auto Manufacturers",
+    sic: "3711",
+    sicSector: "Manufacturing",
+    sicIndustry: "Motor Vehicles & Passenger Car Bodies",
+    famaSector: "",
+    famaIndustry: "Automobiles and Trucks",
+    currency: "USD",
+    location: "California; U.S.A",
+    api_id: "eaeafc4ffc04a49da153adebf1f6960a"
+  }
+]
+
+
 # # Connect and authenticate with your Algolia app
 # client = Algolia::Search::Client.create('K0X97I2FU8', '8a7a37583a0a64ec1bad468b42e92342')
 
@@ -31,53 +74,10 @@ client = Algolia::Search::Client.create('K0X97I2FU8', '8a7a37583a0a64ec1bad468b4
 index = client.init_index("TakeStock")
 
 ## Save your records => This must be a JSON format - Generates an Object ID if one does not already exist)
-index.save_objects(records, { autoGenerateObjectIDIfNotExist: true })
-
+# json_records = JSON.parse(records.to_json)
+# index.save_objects(records, { autoGenerateObjectIDIfNotExist: true })
 
 # # Search the index and print the results
-query = form.input.value
-results = index.search(query)
+# query = form.input.value
+results = index.search("TES")
 puts results[:hits][0]
-
-
-# TEST RECORDS
-# records = [
-#   {
-#     name: "APPLE INC",
-#     ticker: "AAPL",
-#     cik: "320193",
-#     cusip: "037833100",
-#     exchange: "NASDAQ",
-#     isDelisted: false,
-#     category: "Domestic Common Stock",
-#     sector: "Technology",
-#     industry: "Consumer Electronics",
-#     sic: "3571",
-#     sicSector: "Manufacturing",
-#     sicIndustry: "Electronic Computers",
-#     famaSector: "",
-#     famaIndustry: "Computers",
-#     currency: "USD",
-#     location: "California; U.S.A",
-#     api_id: "a43c3ffca9b4a0be9cee4fa1120832a2"
-#   },
-#   {
-#     name: "TESLA INC",
-#     ticker: "TSLA",
-#     cik: "1318605",
-#     cusip: "88160R101",
-#     exchange: "NASDAQ",
-#     isDelisted: false,
-#     category: "Domestic Common Stock",
-#     sector: "Consumer Cyclical",
-#     industry: "Auto Manufacturers",
-#     sic: "3711",
-#     sicSector: "Manufacturing",
-#     sicIndustry: "Motor Vehicles & Passenger Car Bodies",
-#     famaSector: "",
-#     famaIndustry: "Automobiles and Trucks",
-#     currency: "USD",
-#     location: "California; U.S.A",
-#     api_id: "eaeafc4ffc04a49da153adebf1f6960a"
-#   }
-# ]
