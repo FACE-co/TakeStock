886022adc8b4d06ece854fa9cdea427f298267de
Ambery Harris <amberyharris.ah@gmail.com>
2022-12-05 13:17:10 +1100

    twitter restructuring done

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,24 @@
 class TweetsController < ApplicationController
   def index
-    @stock = Stock.friendly.find(params[:id])
+    @stock = Stock.friendly.find(params[:stock_id])
     @tweets = get_tweet_ids(@stock)
   end
 
   private
 
   def get_tweet_ids(stock)
-    query = "https://api.twitter.com/2/tweets/search/recent?query=#{stock.ticker}"
-    query_serialized = URI.open(query).read
-    query_parsed = JSON.parse(query_serialized)
+    url = URI("https://api.twitter.com/2/tweets/search/recent?query=#{stock.ticker}")
 
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
 
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "Bearer #{ENV['TWITTER_BEARER_TOKEN']}"
+    # request["Cookie"] = "guest_id=v1%3A166993649185035697; guest_id_ads=v1%3A166993649185035697; guest_id_marketing=v1%3A166993649185035697; personalization_id=\"v1_RFCmy9nYtnpzm8vj/loxZw==\""
+
+    response = https.request(request).read_body.force_encoding("UTF-8")
+    id_array = JSON.parse(response)["data"].map {|hash| hash["id"]}
+
+    return id_array
   end
 end
--- app/javascript/controllers/twitter_controller.js
+++ app/javascript/controllers/twitter_controller.js
@@ -5,28 +5,20 @@ export default class extends Controller {
   connect() {
     console.log("twitter ok")
 
-    const query = `https://api.twitter.com/2/tweets/search/recent?query=${this.data.get("ticker")}`
-    console.log(query)
+    const ids = JSON.parse(this.data.get("tweets"))
+    console.log(ids)
 
-    const params = {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer '+ "AAAAAAAAAAAAAAAAAAAAAFKOjwEAAAAAc6ZcYma1nuXCiCsxrW041C%2BsR%2Fc%3DMV1FvaN9QRiL212vBpmKhWHQ2eDYZmrWznM6By8WTfUCeE7Avk",
-      }
-    };
-
-    fetch(query, params)
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data)
+    window.onload = () => {
+      ids.forEach((id) => {
+        console.log(id)
+        twttr.widgets.createTweet(
+          id,
+          document.getElementById("tweet"),
+          {
+            theme: "light"
+          }
+        );
       });
-
-    twttr.widgets.createTweet(
-      "1598050795882442752",
-      document.getElementById("tweet"),
-      {
-        theme: "light"
-      }
-    );
+    };
   }
 }
