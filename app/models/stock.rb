3d56860a563ca887634713be314d95bbf27597e2
Farah Masri <farahmasri_97@hotmail.com>
2022-12-06 12:07:19 +1100

    trending added to stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
   include ActionView::Helpers::NumberHelper
+  # include AlgoliaSearch
 
-  has_many :portfolio_stocks
   has_many :portfolios, through: :portfolio_stocks
   extend FriendlyId
   friendly_id :ticker, use: :slugged
@@ -9,19 +9,48 @@ class Stock < ApplicationRecord
 
   before_validation :upcase_ticker
 
+  # algoliasearch do
+  #   # Use all default configuration
+  # end
+
   def upcase_ticker
     self.ticker = self.ticker.upcase
   end
 
-  def trending
-    resp = RestClient::Request.execute(
-      :method => :get,
-      :url => "https://api.stockgeist.ai/stock/us/hist/message-metrics?symbols=#{self.ticker}&start=2022-12-03T00%3A00&end=2022-12-04T00%3A00&metrics=pos_total_count",
-      :headers => {Accept: "application/jsonl",
-                  token: "lOvHMrdlqwhHRkVU0UZi3PDUI9njSsPU"}
-      )
-    response = JSON.parse(resp.body)
-    pos_count = response['data'][self.ticker][0]['pos_total_count']
-    return pos_count
+  def basic_info
+    api_key = "R4E0Q2VIZSLUWL6Q"
+
+    # getting fundamental data
+    query_fundamental = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{self.ticker}&apikey=#{api_key}"
+    stock_fundamental = URI.open(query_fundamental).read
+    stock_fundamental_hash = JSON.parse(stock_fundamental)
+
+    # getting daily price data
+    query_daily = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=#{self.ticker}&apikey=#{api_key}"
+    stock_daily = URI.open(query_daily).read
+    stock_daily_hash = JSON.parse(stock_daily)
+    stock_daily_hash_only_first_two_days = stock_daily_hash["Time Series (Daily)"].first(2).to_h
+    stock_today = stock_daily_hash_only_first_two_days.first[1]
+
+    # calculating change and change%
+    stock_today_close = stock_daily_hash_only_first_two_days.values[0]["4. close"].to_f
+    stock_yesterday_close = stock_daily_hash_only_first_two_days.values[1]["4. close"].to_f
+    change = stock_yesterday_close - stock_today_close
+    change_percentage = change / stock_yesterday_close
+
+    # making change/change% a hash
+    temp_hash = {}
+    temp_hash["change"] = "#{change.round(2)}"
+    temp_hash["change%"] = "#{change_percentage.round(2)}%"
+
+    final_hash = stock_fundamental_hash.merge(stock_today).merge(temp_hash)
+
+    return final_hash
+  end
+
+  def yahooapi
+    query = BasicYahooFinance::Query.new
+    data = query.quotes(self.ticker)
+    return data["#{self.ticker}"]
   end
 end
