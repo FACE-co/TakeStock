a6b027e129d3eed28804f3f39334e1a5ea6de829
Ambery Harris <amberyharris.ah@gmail.com>
2022-12-07 23:22:08 +1100

    reddit subreddit icon requesting logic done, very inefficient

--- app/services/reddit_search.rb
+++ app/services/reddit_search.rb
@@ -28,15 +28,31 @@ class RedditSearch < ApplicationService
         :method => :get,
         :url => "https://www.reddit.com/search.json?q=#{@query}&t=week&sort=top",
         :headers => { Authorization: token }
-     )
+      )
+      # json file of reddit articles
+      reddit_hash = JSON.parse(raw)
+      posts = reddit_hash['data']['children']
+      # parsed json into ruby hash
+      # returns hash of hashes post via ['data']['children']
 
-     # json file of reddit articles
-     reddit_hash = JSON.parse(raw)
+      # icons_urls = []
 
-     # parsed json into ruby hash
-     # returns hash of hashes post via ['data']['children']
-     articles = reddit_hash['data']['children']
-     return articles
+      # articles.each do |article|
+      #   icons = RestClient::Request.execute(
+      #     :method => :get,
+      #     :url => "https://www.reddit.com/r/#{article['data']['subreddit']}/about.json",
+      #     :headers => { Authorization: token }
+      #     )
+
+      #   icons_hash = JSON.parse(icons)
+      #   icon_url = icons_hash['icon_img']
+      #   icons_urls.append(icon_url)
+      #  end
+
+       articles = posts
+      #  articles = [posts, icons_urls]
+       return articles
+
 
     rescue StandardError => e
       raise StandardError.new 'Error getting Reddit articles.'
