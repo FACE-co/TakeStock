c054d63f3087a8a82238cbabf8861b3191b7f77b
Farah Masri <farahmasri_97@hotmail.com>
2022-12-08 10:49:52 +1100

    first commit for news cards on front page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,5 +8,17 @@ class DashboardController < ApplicationController
     else
       @stocks = Stock.all
     end
+
+    @news = news
+  end
+
+  private
+
+  def news
+    query = "https://newsapi.org/v2/everything?q=investment&lang=en&sortBy=popularity&apiKey=#{ENV['NEWS_API_KEY']}"
+    # query = "https://newsapi.org/v2/everything?q=#{stock.ticker}&from=#{enddate}&to=#{enddate}&sortBy=popularity&apiKey=#{ENV['NEWS_API_KEY']}"
+    stock_news = URI.open(query).read
+    news_hash = JSON.parse(stock_news)
+    return news_hash
   end
 end
