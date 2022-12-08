3a33401d6497a64b6048863fc498a67e3d2a09e6
Farah Masri <farahmasri_97@hotmail.com>
2022-12-08 19:04:08 +1100

    commit just for fun

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -55,33 +55,19 @@ module.exports = {
         "4xl": "2.25rem",
         "5xl": "3rem",
         "6xl": "4rem",
+        "7xl": "4.5rem",
+        "9xl": "8rem",
       },
       boxShadow: {
         "inner-enhanced": 'inset 0 0 5px 2px rgb(0 0 0 / 0.15)',
-      }
-    },
       },
       display: ["group-hover"],
-    },
-    fontSize: {
-      xxs: "0.7rem",
-      xs: "0.75rem",
-      sm: "0.875rem",
-      base: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "3xl": "1.875rem",
-      "4xl": "2.25rem",
-      "5xl": "3rem",
-      "6xl": "4rem",
-      "7xl": "4.5rem",
-      "9xl": "8rem",
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
   ]
 }
