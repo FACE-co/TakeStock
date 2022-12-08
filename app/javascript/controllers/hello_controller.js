37e4c3b3c6fb1c7137faa33bb9966a29849bec86
Caaasey <baimaxsolution@gmail.com>
2022-12-09 09:17:31 +1100

    code refactor

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,18 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["flashes"]
   connect() {
-    let fadeEffect = setInterval(function () {
-      if (this.flashesTarget.style.opacity) {
-          this.flashesTarget.style.opacity = 1;
-      }
-      if (this.flashesTarget.style.opacity > 0) {
-          this.flashesTarget.style.opacity -= 0.1;
-      } else {
-          clearInterval(fadeEffect);
-      }
-  }, 200);
   }
 
 }
